2fe521201333d495edc00c8afb6f4399a69d6d3e
Suhani <suhanisg200@gmail.com>
2026-03-20 21:31:31 +0530

    fixed auth localhost issue

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -17,12 +17,11 @@ export const AuthProvider = ({ children }) => {
 
     const login = async (email, password) => {
         try {
-            const response = await fetch('http://localhost:5000/api/login', {
+            const response = await fetch('https://polyvo.onrender.com/api/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password }),
             });
-
             const data = await response.json();
 
             if (!response.ok) {
